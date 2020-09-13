b00c2231fb321d9a65556cd0b2b6d845865536a7
engineve <yeindap@gmail.com>
2020-09-13 21:03:47 +0900

    CREATE arrBasic.cpp
    const, array concept

---
+++ arrBasic.cpp
@@ -0,0 +1,19 @@
+// #include "stdafx.h"
+#include<iostream>
+
+const char MAXSIZE=30;
+
+int main(void)
+{
+printf("test \n");	
+
+char arr[MAXSIZE];
+//std::cin>>arr;
+std::cin.getline(arr,sizeof(arr)); 
+std::cout<<"result : "<<std::endl<<arr;   
+}
+
+// test 
+// nice to meet you
+// result : 
+// nice to meet you
