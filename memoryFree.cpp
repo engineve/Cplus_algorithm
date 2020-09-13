12fadbdeabca4d3242e45a58b2ac43943f99bb12
engineve <yeindap@gmail.com>
2020-09-13 19:54:59 +0900

    CREATE tasks.json, launch.json
    c++ setting

--- memoryFree.cpp
+++ memoryFree.cpp
@@ -2,8 +2,9 @@
 #include<stdlib.h>
 int main(void)
 {
-int *num;
-num=(int*)malloc(sizeof(int));  //sizeof(int) -> 4byte memory allocation, 4byte address allocated to num
-*num=10;
-printf("result=%d\n",*num);
-free(a); // cancel the allocation
+    int *num;
+    num=(int*)malloc(sizeof(int));  //sizeof(int) -> 4byte memory allocation, 4byte address allocated to num
+    *num=10;
+    printf("result=%d\n",*num); // result=10
+    free(num); // cancel the allocation
+}
